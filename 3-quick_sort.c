90157373e27eedadbd2faae7e1a70916d215a112
Imarino4 <ignacio.martacos@gmail.com>
2022-08-30 16:39:06 -0700

    Tsak 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,11 +10,18 @@ void quick_sort(int *array, size_t size)
 	quicks(array, 0, size - 1, size);
 }
 
+/**
+ * quicks - auxiliar function to make recursion
+ * @array: array to be sorted
+ * @min: min index of the subarray
+ * @max: max indec of the subarray
+ * @size: array's size
+ */
 void quicks(int *array, int min, int max, size_t size)
 {
 	int index = 0;
 /*Caso base cuando hay un único elemento -> min == max*/
-	while (min < max)
+	if (min < max)
 	{
 		index = lomuto_partition(array, min, max, size);
 		quicks(array, min, index - 1, size);
@@ -22,6 +29,14 @@ void quicks(int *array, int min, int max, size_t size)
 	}
 }
 
+/**
+ * lomuto_partition - implementation of lomuto algorithm to partionate an array
+ * @array: array to be partionated
+ * @min: min index of the subarray
+ * @max: max indec of the subarray
+ * @size: array's size
+ * Return: index of partition
+ */
 int lomuto_partition(int *array, int min, int max, size_t size)
 {
 	int pivot = array[max], i = min - 1, j = min, temp = 0;
@@ -33,7 +48,6 @@ int lomuto_partition(int *array, int min, int max, size_t size)
 			i++;
 			if (i < j)
 			{
-				printf("pivot = %d\n", pivot);
 				temp = array[j];
 				array[j] = array[i];
 				array[i] = temp;
